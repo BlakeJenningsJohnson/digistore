c7904ec950133a55844f4bddbd4532f25919b425
Your Name <blake.jennings@gmail.com>
2014-03-12 10:08:17 -0700

    added table for cart display

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -20,32 +20,32 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = '', stack1, helper, options;
-  data.buffer.push("\n        <ul>\n          <li><h4>");
-  stack1 = helpers._triageMustache.call(depth0, "product.name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
-  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("</h4></li>\n\n          <li><img ");
+  data.buffer.push("\n        <thead>\n          <tr>\n            <th>Product</th>\n            <th></th>\n            <th>Price</th>\n            <th>Quantity</th>\n            <th>Subtotal</th>\n          </tr>\n        </thead>\n        <tbody>\n          <tr>\n            <td><img ");
   data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
     'src': ("product.avatar")
   },hashTypes:{'src': "ID"},hashContexts:{'src': depth0},contexts:[],types:[],data:data})));
-  data.buffer.push(" class='thumbnail'></li>\n          <li><h4>");
+  data.buffer.push(" class='thumbnail'></td>\n            <td>  ");
+  stack1 = helpers._triageMustache.call(depth0, "product.name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push(" </td>\n            <td>$");
+  data.buffer.push(escapeExpression((helper = helpers['format-price'] || (depth0 && depth0['format-price']),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "product.price", options) : helperMissing.call(depth0, "format-price", "product.price", options))));
+  data.buffer.push("</td>\n            <td>\n              ");
   stack1 = helpers._triageMustache.call(depth0, "quantity", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("</h4></li>\n          <button ");
+  data.buffer.push("\n              <button ");
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "more", "", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0,depth0],types:["STRING","ID"],data:data})));
-  data.buffer.push(">+</button>\n          <button ");
+  data.buffer.push(">+</button>\n              <button ");
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "less", "", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0,depth0],types:["STRING","ID"],data:data})));
-  data.buffer.push(">-</button>\n          <p>Price: $");
-  data.buffer.push(escapeExpression((helper = helpers['format-price'] || (depth0 && depth0['format-price']),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "product.price", options) : helperMissing.call(depth0, "format-price", "product.price", options))));
-  data.buffer.push(" </p>\n          <p>Subtotal: $");
+  data.buffer.push(">-</button>\n            </td>\n            <td>$");
   data.buffer.push(escapeExpression((helper = helpers.subtotal || (depth0 && depth0.subtotal),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0,depth0],types:["ID","ID"],data:data},helper ? helper.call(depth0, "quantity", "product.price", options) : helperMissing.call(depth0, "subtotal", "quantity", "product.price", options))));
-  data.buffer.push(" </p>\n        </ul>\n    ");
+  data.buffer.push("</td>\n          </tr>\n          ");
   return buffer;
   }
 
-  data.buffer.push("<h1>Cart</h1>\n <h3>All Items</h3>\n    <div class=\"row\">\n      ");
+  data.buffer.push("<h1>Cart</h1>\n  <div class=\"table-responsive\">\n    <table class=\"table table-bordered\">\n      ");
   stack1 = helpers.each.call(depth0, "items", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n\n");
+  data.buffer.push("\n        </tbody>\n      </table>\n    </div>");
   return buffer;
   
 });
