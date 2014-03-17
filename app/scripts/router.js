b1054328b66d673ecffe9b0cff35a3d5d026966e
Your Name <blake.jennings@gmail.com>
2014-03-17 14:17:42 -0700

    deleted comments, added /order for confirmation route

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,7 +2,7 @@ App.Router.map(function(){
   this.route("home", { path: "/" });
   this.resource("cart");
   this.resource("order");
-  this.resource('confirmation', { path: ':order_id' });
+  this.resource('confirmation', { path: 'order/:order_id' });
   this.resource("products", function() {
     this.resource('product', { path: ':product_id' });
   });
--- app/scripts/routes/order.js
+++ app/scripts/routes/order.js
@@ -1,13 +1,11 @@
 App.OrderRoute = Ember.Route.extend({
   actions: {
     submitBilling: function (attributes) {
-      // var theCart = this.store.find('cart', 1)
-      var order = this.store.createRecord('order', attributes) /*attach to cart?*/
+      var order = this.store.createRecord('order', attributes) 
       var self = this
-      // order.setProperties({ status: "pending", cart: theCart });
       order.save().then(
         function (order) {
-        self.transitionTo('confirmation', order); //order.get('id')?
+        self.transitionTo('confirmation', order);
       }, 
       function (error) {
         order.deleteRecord()
