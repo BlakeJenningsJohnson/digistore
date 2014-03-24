06b293a8264af5e259b3aa7a18bf537c65f927a9
Your Name <blake.jennings@gmail.com>
2014-03-23 21:46:49 -0700

    added deleteOrder and updateOrder admin side

--- app/scripts/routes/manageOrders.js
+++ app/scripts/routes/manageOrders.js
@@ -2,4 +2,16 @@ App.ManageOrdersRoute = Ember.Route.extend({
   model: function () {
     return this.store.find('order');
   },
-})
+  actions: {
+    updateOrder: function(order) {
+      order.save();
+      alert("Status has been updated.");
+      this.transitionTo('manageOrders');
+    },
+    deleteOrder: function (order) {
+      order.deleteRecord();
+      order.save();
+      this.transitionTo('manageOrders');
+    }    
+  }
+});
